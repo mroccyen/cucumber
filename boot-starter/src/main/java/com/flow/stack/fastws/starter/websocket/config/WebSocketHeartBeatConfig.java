0fab597f6786b467d70801961f4a6fd340d84f2e
mroccyen <qingshanpeng@126.com>
2020-08-12 23:50:52 +0800

    1、优化内存管理代码 2、添加心跳检测额外操作 3、调整项目结构，增加core模块和starter模块

---
+++ boot-starter/src/main/java/com/flow/stack/fastws/starter/websocket/config/WebSocketHeartBeatConfig.java
@@ -0,0 +1,61 @@
+package com.flow.stack.fastws.starter.websocket.config;
+
+import com.flow.stack.fastws.starter.websocket.WebSocketManager;
+import com.flow.stack.fastws.starter.websocket.heartbeat.WebSocketHeartBeatChecker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
+
+/**
+ * 只需要配置一下三个选项即可开启心跳监测
+ * flow.fastws.webSocket.heartCheck.enabled=true
+ * flow.fastws.webSocket.heartCheck.timeSpan=1000
+ * flow.fastws.webSocket.heartCheck.errorToleration=30
+ *
+ * @author qingsp
+ * @version 0.0.1
+ * @date 2020-08-06
+ */
+@Configuration
+@ConditionalOnProperty(prefix = "flow.fastws.webSocket.heartCheck", name = "enabled", havingValue = "true")
+public class WebSocketHeartBeatConfig {
+
+    @Value("${flow.fastws.webSocket.heartCheck.timeSpan:10000}")
+    private long timeSpan;
+
+    @Value("${flow.fastws.webSocket.heartCheck.errorToleration:30}")
+    private int errorToleration;
+
+    private final WebSocketManager webSocketManager;
+
+    private final WebSocketHeartBeatChecker webSocketHeartBeatChecker;
+
+    @Autowired
+    public WebSocketHeartBeatConfig(WebSocketManager webSocketManager,
+                                    WebSocketHeartBeatChecker webSocketHeartBeatChecker) {
+        this.webSocketManager = webSocketManager;
+        this.webSocketHeartBeatChecker = webSocketHeartBeatChecker;
+    }
+
+    /**
+     * 定时检测 WebSocket 的心跳
+     */
+    @Scheduled(cron = "${flow.fastws.webSocket.heartCheck.trigger}")
+    public void webSocketHeartCheckJob() {
+        webSocketHeartBeatChecker.check(webSocketManager, timeSpan, errorToleration,
+                webSockets -> {
+                    if (WebSocketHeartBeatChecker.listener != null) {
+                        //失去心跳后执行额外的操作
+                        WebSocketHeartBeatChecker.listener.todoAtRemoved(webSockets);
+                    }
+                },
+                webSocket -> {
+                    if (WebSocketHeartBeatChecker.listener != null) {
+                        //心跳检测时执行额外的操作
+                        WebSocketHeartBeatChecker.listener.todoAtChecking(webSocket);
+                    }
+                });
+    }
+}
---
+++ boot-starter/src/main/java/com/flow/stack/fastws/starter/websocket/config/memory/MemoryWebSocketConfig.java
@@ -0,0 +1,27 @@
+package com.flow.stack.fastws.starter.websocket.config.memory;
+
+import com.flow.stack.fastws.starter.websocket.WebSocketManager;
+import com.flow.stack.fastws.starter.websocket.config.WebSocketConfig;
+import com.flow.stack.fastws.starter.websocket.endpoint.WebSocketEndpointImpl;
+import com.flow.stack.fastws.starter.websocket.memory.MemWebSocketManager;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+
+/**
+ * 内存管理webSocket配置
+ *
+ * @author qingsp
+ * @version 0.0.1
+ * @date 2020-08-06
+ */
+@Import({WebSocketConfig.class, WebSocketEndpointImpl.class})
+public class MemoryWebSocketConfig {
+
+    @Bean(WebSocketManager.WEBSOCKET_MANAGER_NAME)
+    @ConditionalOnMissingBean(name = WebSocketManager.WEBSOCKET_MANAGER_NAME)
+    public WebSocketManager webSocketManager() {
+        return new MemWebSocketManager();
+    }
+
+}
